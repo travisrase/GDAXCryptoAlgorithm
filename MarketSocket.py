822c26cbe01053b9792613d57059d57c3bd57e26
maxtanous <MaxTanous@dhcp-217-133.wireless.bowdoin.edu>
2017-11-30 11:00:25 -0500

    f

--- MarketSocket.py
+++ MarketSocket.py
@@ -27,4 +27,33 @@ class MarketSocket:
 
 
         return resp.json().get("price")
+
+    def getMarketVolume24(self):
+
+
+        auth = self.exchangeAuth
+        resp = requests.get(self.endpoint, auth=auth )
+
+        if resp.status_code != 200:
+            print(resp.status_code)
+                      # This means something went wrong.
+            raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+
+        return resp.json().get("volume")
+
+    def getMarketSize(self):
+
+
+        auth = self.exchangeAuth
+        resp = requests.get(self.endpoint, auth=auth )
+
+        if resp.status_code != 200:
+            print(resp.status_code)
+                      # This means something went wrong.
+            raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+
+        return resp.json().get("size")
+
         #print(resp.json.get("price"))
