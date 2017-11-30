cc5b55f9dcd1ef28271f98b714835a27f0da7fc3
maxtanous <MaxTanous@dhcp-222-7.wireless.bowdoin.edu>
2017-11-29 22:44:20 -0500

    main test-buy

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ class main:
 #	import Purchase
 	import CoinBaseExchangeAuth
 	import MarketSocket
-	import Time
+	import time
 
-	global acceptable_loss,  total_loss, clock
+	global acceptable_loss,  total_loss, clock, market, coinbase_terminal
 
 	#User Input information for keys
-	#User_API_Key = input("Input the API KEY: ")
-	#User_Passphrase = input("Input the Passphrase: ")
-	#User_Secret= input("Input the secret Key: ")
+	User_API_Key = input("Input the API KEY: ")
+	User_Passphrase = input("Input the Passphrase: ")
+	User_Secret= input("Input the secret Key: ")
+	dollar_buy = input("How much do you want to spend?: ")
+	setPrice = input("What is the price you want to buy at? :")
 
 	#buy terminal
 
@@ -21,7 +23,7 @@ class main:
 
 
 
-	coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth(api_key,api_secret, api_passphrase)
+	coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
 	#coinbase_terminal = CoinbaseExchangeAuth(User_API_Key, User_Secret, User_Passphrase)
 	market = MarketSocket.MarketSocket(coinbase_terminal, 'https://api.gdax.com/products/ltc-usd/ticker')
 
@@ -32,12 +34,27 @@ class main:
 
 	while True: # conditions for running the program
 
+		#time interval code
+		if market.getMarketPrice() >= setPrice:
+
+			size1 = setPrice * dollar_buy
+			test = CoinbaseExchangeAuth(api, secret, passphrase)
+
+			api_url = 'https://api.gdax.com/'
+			auth = coinbase_terminal
+			r = requests.get(api_url + 'accounts', auth=auth)
+
+			order = {
+			      'size': size1,
+			      'price': setPrice,
+			      'side': 'buy',
+			      'product_id': 'LTC-USD',
+			   	}
+
+			r = requests.post(api_url + 'orders', json=order, auth=auth)
 
 
 
-		#Pull data from gdax
 
-		#Algorithm:
-			#when conditions are met create purchase object for given coin
 
 main()
