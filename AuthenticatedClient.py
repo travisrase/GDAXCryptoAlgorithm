c09586cb8cacf43a8547f9ba9d1f63c1e913f358
travisrase <34067663+travisrase@users.noreply.github.com>
2017-11-30 11:11:14 -0500

    Update AuthenticatedClient.py

--- AuthenticatedClient.py
+++ AuthenticatedClient.py
@@ -9,7 +9,23 @@ class AuthenticatedClient:
       def __init__(self, CoinBaseExchangeAuth):
         self.auth = CoinBaseExchangeAuth
        
-      
-     def buy()
+     #size and price may be given as ints or strings, product_id must be a string
+     def buy(size, price, product_id):
+      size = str(size)
+      price = str(price)
+      api_url = 'https://api.gdax.com/'
+			r = requests.get(api_url + 'accounts', auth=auth)
+
+			order = {
+			      'size': size,
+			      'price': price,
+			      'side': 'buy',
+			      'product_id': product_id,
+			   	}
+
+			r = requests.post(api_url + 'orders', json=order, auth=auth)
+			print(r.status_code)
+			print("------------------BOUGHT-------------")
+			print(order)
 
   
