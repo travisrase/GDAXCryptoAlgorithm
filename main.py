723e34f516fcfab2f467c42b04cd0c49af593633
maxtanous <MaxTanous@dhcp-217-133.wireless.bowdoin.edu>
2017-12-01 16:28:17 -0500

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-import CoinBaseExchangeAuth
+from CoinBaseExchangeAuth import CoinbaseExchangeAuth
 import MarketSocket
 import requests
 from threading import Timer
@@ -32,7 +32,7 @@ class main:
 	api_url = 'https://api.gdax.com/'
 
 	#coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
-	coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth("08212010cbe18d41564ee6cb19a846dd","kLat+yAxLlJETvk7nKsfsoLoSTKgA2wSv1rCAcdqLwIWx2OYwTON2InUUb6wJ8sv5Y/kUcgx9sTOgSqrhARvlQ==", "xlce5thdgi")
+	coinbase_terminal = CoinbaseExchangeAuth()
 
 
 	market = MarketSocket.MarketSocket(coinbase_terminal, 'https://api.gdax.com/products/ltc-usd/ticker')
@@ -40,6 +40,8 @@ class main:
 	auth_client = AuthenticatedClient(coinbase_terminal, api_url)
 
 	algo = Algorithm("dummy", auth_client, market, ".01", "LTC-USD")
+
+
 
 	algo.secondDerivBuyAlgo()
 
