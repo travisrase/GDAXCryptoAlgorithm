b4d48e8b269d731e3a81d38c1b4f85b35893ea12
maxtanous <MaxTanous@dhcp-223-70.wireless.bowdoin.edu>
2017-12-03 13:50:07 -0500

    Implement Sell

--- AuthenticatedClient.py
+++ AuthenticatedClient.py
@@ -32,3 +32,24 @@ class AuthenticatedClient:
         print(r.status_code)
 		#print("------------------BOUGHT-------------")
         print(order)
+
+
+    def sell(self, size, price, product_id):
+        size = str(size)
+        price = str(price)
+        r = requests.get(self.api_url + 'accounts', auth=self.auth)
+
+        order = {
+			      'size': size,
+			      'price': price,
+			      'side': 'sell',
+			      'product_id': product_id
+			   	}
+
+
+        r = requests.post(self.api_url + 'orders', json=order, auth=self.auth)
+        print(r.json())
+
+        print(r.status_code)
+		print("------------------SOLD-------------")
+        print(order)
