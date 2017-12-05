2253eb9cbcae773230e5d258b8809cade43ace69
travisrase <34067663+travisrase@users.noreply.github.com>
2017-12-04 20:35:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,32 +16,24 @@ class main:
 	global acceptable_loss,  total_loss, clock, market, coinbase_terminal, setPrice, dollar_buy
 
 	#User Input information for keys
-	#User_API_Key = input("Input the API KEY: ")
-	#User_Secret= input("Input the secret Key: ")
-	#User_Passphrase = input("Input the Passphrase: ")
+	User_API_Key = input("Input the API KEY: ")
+	User_Secret= input("Input the secret Key: ")
+	User_Passphrase = input("Input the Passphrase: ")
 
 	#dollar_buy = float(input("How much do you want to spend?: "))
 	#setPrice = float(input("What is the price you want to buy at? :"))
 
 	#buy terminal
 
-
-
-
-
 	api_url = 'https://api.gdax.com/'
 
-	#coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
-	coinbase_terminal = CoinbaseExchangeAuth()
+	coinbase_terminal = CoinBaseExchangeAuth.CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
+	#coinbase_terminal = CoinbaseExchangeAuth()
 
 
 	market = MarketSocket.MarketSocket(coinbase_terminal, 'https://api.gdax.com/products/ltc-usd/ticker')
-
 	auth_client = AuthenticatedClient(coinbase_terminal, api_url)
-
 	algo = Algorithm("dummy", auth_client, market, ".01", "LTC-USD")
-
-
 
 	algo.secondDerivBuyAlgo()
 
