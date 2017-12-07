44269dbd42fe9a1dee0665f858c60aa2b3de6f06
travisrase <34067663+travisrase@users.noreply.github.com>
2017-12-07 12:44:51 -0500

    Update MarketData.py

--- MarketData.py
+++ MarketData.py
@@ -17,6 +17,52 @@ class MarketData:
     
     
     
+    def getAverageGain(self, numPeriods = self.sizePriceTable, updatePriceTable = True):
+        if(updatePriceTable):
+            self.__fillPriceTable()
+        
+        average = -1
+        gainTable = []
+        if(len(self.priceTable) < numPeriods):
+            return average
+        else:
+            for i in range(1,numPeriods):
+                gain = self.priceTable[i] - self.priceTable[i-1]
+                if(gain > 0):
+                    gainTable.insert(i-1, loss)
+                else:
+                    gainTable.insert(i-1, 0)
+            sm = sum(lossTable)
+            average = sm/numPeriods
+            return average
+            
+    def getAverageLoss(self, numPeriods = self.sizePriceTable, updatePriceTable = True):
+        if(updatePriceTable):
+            self.__fillPriceTable()
+        
+        average = -1
+        lossTable = []
+        if(len(self.priceTable) < numPeriods):
+            return average
+        else:
+            for i in range(1,numPeriods):
+                loss = self.priceTable[i-1] - self.priceTable[i]
+                if(loss > 0):
+                    lossTable.insert(i-1, loss)
+                else:
+                    lossTable.insert(i-1, 0)
+
+            sm = sum(lossTable)
+            average = sm/numPeriods
+            return average
+            
+    
+    def __fillPriceTable(self):
+        self.__updatePriceTable()
+        while(self.__isPriceTableFull() == False):
+            self.__updatePriceTable()
+        
+    
     def __isPriceTableFull(self):
         if(self.priceTable[-1] == -1):
             return False
