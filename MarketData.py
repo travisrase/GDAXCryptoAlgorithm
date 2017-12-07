e8dbb30ac62a79b74a289bf45475b72382a072cc
travisrase <34067663+travisrase@users.noreply.github.com>
2017-12-07 12:19:30 -0500

    Update MarketData.py

--- MarketData.py
+++ MarketData.py
@@ -8,7 +8,26 @@ class MarketData:
         self.MarketSocket = MarketSocket
         self.updateInterval = updateInterval
         self.sizePriceTable = sizePriceTable
-        self.priceTable = []
+        self.priceTable = [-1]*sizePriceTable
+    
+    
+    
+    
+    
+    
+    
+    
+    def __isPriceTableFull(self):
+        if(self.priceTable[-1] == -1):
+            return False
+        else:
+            return True
+    
+    def __updatePriceTable(self):
+
+        self.priceTable.insert(0, float(self.MarketSocket.getMarketPrice()))
+        self.recentPriceTable = self.recentPriceTable[:-1]
+        time.sleep(self.updateInterval)
    
   
    
