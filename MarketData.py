7be9a077fa10350322b8fd321bb2436d90c8eb8e
travisrase <34067663+travisrase@users.noreply.github.com>
2017-12-07 12:12:09 -0500

    Update MarketData.py

--- MarketData.py
+++ MarketData.py
@@ -1,1 +1,14 @@
 
+class MarketData:
+
+
+
+    def __init__(self, MarketSocket, updateInterval, sizePriceTable):
+
+        self.MarketSocket = MarketSocket
+        self.updateInterval = updateInterval
+        self.sizePriceTable = sizePriceTable
+        self.priceTable = []
+   
+  
+   
