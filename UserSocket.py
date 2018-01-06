20fad7ce9c1583228ad102594be5190b5649aa85
MaxTanous <mctanous@bowdoin.edu>
2018-01-06 16:42:22 -0500

    with auth

--- UserSocket.py
+++ UserSocket.py
@@ -1,4 +1,7 @@
-import CoinBaseExchangeAuth
+import json, hmac, hashlib, time, requests, base64
+
+from requests.auth import AuthBase
+
 import MarketSocket
 import requests
 from threading import Timer
@@ -10,7 +13,7 @@ import time
 class UserSocket():
 
 
-    def __init__(self,adress, CoinBaseExchangeAuth):
+    def __init__(self,adress, api_key="", api_secret="", api_passphrase=""):
         self.CoinBaseExchangeAuth = CoinBaseExchangeAuth
         self.socket = None
         self.channels = {"ticker"}
@@ -21,11 +24,26 @@ class UserSocket():
     def openWS(self):
         socket = websocket.WebSocket()
         socket = socket.connect("wss://ws-feed.gdax.com")
-        return socket
+        self.listen()
 
     def subscribe(self):
 
+
         params = {'type': 'subscribe', 'product_ids': ["LTC-USD"], 'channels': ["full"]}
+
+        time = str(time.time())
+        verify = time + 'GET' + '/users/self/verify'
+        verify = verify.encode('ascii')
+
+        hmac_key = base64.b64decode(self.api_secret)
+        signature = hmac.new(hmac_key, message, hashlib.sha256)
+        signature_b64 = signature.digest().encode('base64').strip('\n')
+
+        params['signature'] = signature_b64
+        params['key'] = self.api_key
+        params['passphrase'] = self.api_passphrase
+        params['timestamp'] = timestamp
+
         self.socket = create_connection(self.adress)
         print(self.socket)
         self.socket.send(json.dumps(params))
